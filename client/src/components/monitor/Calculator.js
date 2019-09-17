eb2a742b8faa98eca8ab1415b13a8a71f7c9e585
Boriphuth Saensukphattraka <boriphuth@adminisatorsmbp.bkk.apac.ime.reuters.com>
2019-09-17 10:17:45 +0700

    Update

--- client/src/components/monitor/Calculator.js
+++ client/src/components/monitor/Calculator.js
@@ -8,7 +8,7 @@ class Calculator extends Component {
         } else {
             return orders.map(order => {
                 return (
-                    <li className="text-right text-success title">
+                    <li className="text-right text-success title" key={order.product.productId}>
                         {order.product.productName} x {order.quantity} = {order.product.unitPrice * order.quantity}
                         <button className="btn btn-light btn-sm" onClick={() => this.props.onDelOrder(order.product)} >X</button>
                     </li>

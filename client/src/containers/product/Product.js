639bc1523ba2ab5d8a05daa485db75e351f22502
Boriphuth Saensukphattraka <boriphuth@administrators-MacBook-Pro.local>
2019-09-18 23:57:51 +0700

    Update

--- client/src/containers/product/Product.js
+++ client/src/containers/product/Product.js
@@ -2,17 +2,32 @@ import React, {Component} from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import ProductList from "../../components/product/ProductList";
+import {withRouter} from "react-router-dom";
 import axios from "axios";
 
 class Product extends Component {
   constructor(props) {
     super(props);
     this.state = {products: null};
+    this.delProduct = this.delProduct.bind(this);
+    this.editProduct = this.editProduct.bind(this);
   }
 
   componentDidMount() {
-    axios.get("http://localhost:3001/products").then(res => {
-        this.setState({products : res.data});
+    axios.get("http://localhost:3001/products").then(response => {
+      this.setState({products: response.data});
+    });
+  }
+
+  editProduct(product) {
+    this.props.history.push("products/edit/" + product.id);
+  }
+
+  delProduct(product) {
+    axios.delete("http://localhost:3001/products/" + product.id).then(response => {
+      axios.get("http://localhost:3001/products").then(response => {
+        this.setState({products: response.data});
+      });
     });
   }
 
@@ -20,8 +35,25 @@ class Product extends Component {
     return (
       <div>
         <Header />
-        <div className="container-fluid">
-          <h1>รายการสั่งซื้อ</h1>
+        <div className="container-fluid ">
+          <div className="row">
+            <div className="col-6">
+              <h1>สินค้า</h1>
+            </div>
+            <div className="col-6">
+              <button
+                className="btn btn-success title float-right"
+                onClick={() => this.props.history.push("products/add")}
+              >
+                เพิ่ม
+              </button>
+            </div>
+          </div>
+          <ProductList
+            products={this.state.products}
+            onEditProduct={this.editProduct}
+            onDelProduct={this.delProduct}
+          />
         </div>
         <Footer />
       </div>
@@ -29,4 +61,4 @@ class Product extends Component {
   }
 }
 
-export default Product;
+export default withRouter(Product);
